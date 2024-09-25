75f3e6181722e7199685d6b06a94e447b3e0433b
benasdom <benasdomd@gmail.com>
2024-09-25 18:18:58 +0000

    my chagne

--- src/Showfiles.jsx
+++ src/Showfiles.jsx
@@ -1,4 +1,4 @@
-import { CloseCircleFilled } from '@ant-design/icons'
+import { CloseCircleOutlined } from '@ant-design/icons'
 import React from 'react'
 
 const Showfiles=({pdflink,setshowpdf,mainlogo})=>{
@@ -6,7 +6,7 @@ const Showfiles=({pdflink,setshowpdf,mainlogo})=>{
         <div className="texttit" id="waiting" >
         <div className="viewer">
             <div className="pdfloader">
-                <div className='imgclose'><div className='closesearch' onClick={()=>setshowpdf(false)}>{<CloseCircleFilled/>}</div><a target='_blank' href={pdflink} className="download">Download</a><img className="logopdf" src={mainlogo} width="150" alt=""/></div>
+                <div className='imgclose'><div className='closesearch' onClick={()=>setshowpdf(false)}>{<CloseCircleOutlined/>}</div><a target='_blank' href={pdflink} className="download">Download</a><img className="logopdf" src={mainlogo} width="150" alt=""/></div>
     <iframe src={pdflink} className="loadpdf" id="loadtext" data-text="Loading...."> Loading....</iframe>
             </div>
          </div>  
