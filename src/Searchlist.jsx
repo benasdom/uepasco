4c8f49d3d7bcbf71e57caab159b0ce6947765d09
benasdom <ben.k.asumadu@gmail.com>
2025-06-27 23:23:55 +0000

    message

--- src/Searchlist.jsx
+++ src/Searchlist.jsx
@@ -83,7 +83,7 @@ fetch(textres).then(res=>res.text()).then(res=>console.log(res))
                     return a=<div className="filtered" key={b+""} data-ptext="title..." title={a.description.replace("-",",")} data-texts="details..."><img src={pdfpic} alt="" className="imgthumb"/>
                     <div className="pinfo">
                         <div className="titles">{(a.description).replace(/o/gi,["⚾","⚽"][b%2])}</div><div className="describe">{a.createdOn}</div>
-                        </div><div href={a.downloadLink} onClick={(ev) => fix(ev.target.attributes.href.value)} className="download">{<ExportOutlined style={{marginRight:"5px"}}/>} open</div></div> })
+                        </div><div href={a.downloadLink} onClick={(ev) => fix(ev.target.attributes.href.value)} className="download">{<ExportOutlined style={{marginRight:"5px"}}/>} open<span className='prema'></span></div></div> })
                     :new Array(1).fill("").map((a,b)=>{
                     return a=<div key={b+""} className="filtered mn4" data-ptext="title..." data-texts="details..."><img src="" alt="" className="imgthumb"/><div className="desc err4">
                         <img className="reglate" src={mainlogo} style={{marginRight:10}} alt=""/>{NetworkError}</div></div>
