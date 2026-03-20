f853430d3d0b7efbfd770645a4735a6f5add0287
benasdom <ben.k.asumadu@gmail.com>
2026-03-20 14:03:50 +0000

    empty search

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 const Search=({setsearching,setfind,bar,eprop,handleMenu})=>{
     const [spin, setspin] = useState(false)
@@ -9,13 +9,21 @@ const Search=({setsearching,setfind,bar,eprop,handleMenu})=>{
             if(lcontent) lcontent.style.cssText="pointer-events:all;clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);";
             const mcomp =document.querySelector(".menucomp");
             if(mcomp) mcomp.style.cssText="pointer-events:none;clip-path: polygon(0 0, 0% 0, 0% 100%, 0 100%);";
-
     }
+    useEffect(() => {
+     setfind("")
+    }, [])
+    
     return(
     <div className="twwo" title="search">
                     <div className="search" onClick={handleSearch}><i className="fa fa-search"></i></div>
                     
-                    <div className="input" onClick={handleSearch}><input type="text" style={{pointerEvents:eprop}} ref={bar} onChange={(e)=>{return setfind(e.target.value.trim(""))}}  className="find" placeholder="Search a course code..."/></div>
+                    <div className="input" onClick={handleSearch}>
+                        <input type="text"
+                        style={{pointerEvents:eprop}}
+                        ref={bar} onChange={(e)=>{return setfind(e.target.value.trim(""))}}
+                        className="find"
+                        placeholder="Search a course code..."/></div>
                     <div className="slash" onClick={handleMenu}>{eprop!="all"?"/":"♒"}</div>
 
      </div>
